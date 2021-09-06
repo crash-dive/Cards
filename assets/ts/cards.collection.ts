d3bdc272c71a7f294fd28cdf066d86e255a569dc
Jonathon Smith <failing@crashdive.co.uk>
2021-09-06 16:58:12 +0100

    Added All Interactivity To Game Page
    Need to do a final format pass on the pages before moving onto about and contact

--- assets/ts/cards.collection.ts
+++ assets/ts/cards.collection.ts
@@ -1,8 +1,8 @@
 import { parse, ParseResult, ParseMeta } from 'papaparse';
 import Tabulator from 'tabulator-tables';
 
-let table = document.getElementById('card-table') as HTMLTableElement;
-let url = table.dataset['url'];
+const table = document.getElementById('table') as HTMLTableElement;
+const url = table.dataset['url'];
 
 parse(url, {
 	download: true,
@@ -11,13 +11,26 @@ parse(url, {
 		if (results.errors.length === 0) {
 			console.log('Parsing complete:', results, file);
 
-            new Tabulator(table, {
+            const tablulator = new Tabulator(table, {
                 data: results.data,
                 height: '100%',
                 layout:'fitColumns',
+                responsiveLayout: 'hide',
+                headerHozAlign: 'center',
+                headerFilterPlaceholder: 'Filter',
+                placeholder: "No Results",
+                initialFilter: [{
+                    field: 'Col',
+                    type: '=',
+                    value: false
+                }],
                 groupBy: groupBy(results.meta.fields),
-                columns: columnDefs(results.meta.fields)
+                groupToggleElement: 'header',
+                columns: columns(results.meta.fields)
             })
+
+            enableFilter(tablulator);
+            enableDownload(tablulator);
 		}
 	}
 });
@@ -32,38 +45,186 @@ function groupBy(fields: string[]): string {
     }
 }
 
-function columnDefs(fields: string[]): Tabulator.ColumnDefinition[] {
-    let columns: Tabulator.ColumnDefinition[] = [];
+function columns(fields: string[]): Tabulator.ColumnDefinition[] {
+    const columns: Tabulator.ColumnDefinition[] = [];
     
     for (const field of fields) {
         if (field === 'Set #') {
             continue;
         }
         else {
-            let column: Tabulator.ColumnDefinition = {
-                title: field,
-                field: field,
-            };
-
-            if (
-                field === 'Set' ||
-                field === 'Type' ||
-                field === 'Rarity'
-            ) {
-                column.headerFilter = 'select';
-                column.headerFilterPlaceholder = 'Filter';
-                column.headerFilterParams = {
-                    values: true
-                };
-            }
-            else {
-                column.headerFilter = 'input';
-                column.headerFilterPlaceholder = 'Filter';
-            }
-
-            columns.push(column);
+            columns.push(column(field));
         }
     }
 
     return columns;
+}
+
+function column(field: string): Tabulator.ColumnDefinition {
+    switch (field) {
+        case 'Set':
+            return {
+                title: field,
+                field: field,
+                headerTooltip: 'The set that the card belongs to',
+                responsive: 100,
+                widthGrow: 1,
+                headerFilter: 'select',
+                headerFilterParams: {
+                    values: true,
+                    sortValuesList: 'asc'
+                }
+            };
+    
+        case 'Card #':
+            return {
+                title: field,
+                field: field,
+                headerTooltip: 'The card\'s number',
+                responsive: 0,
+                widthGrow: 0.5,
+                headerFilter: 'input',
+            };
+
+        case 'Card':
+            return {
+                title: field,
+                field: field,
+                headerTooltip: 'The card\'s name',
+                responsive: 0,
+                widthGrow: 2,
+                headerFilter: 'input',
+            };
+
+        case 'Type':
+            return {
+                title: field,
+                field: field,
+                headerTooltip: 'The type or catagory of the card',
+                responsive: 50,
+                widthGrow: 1,
+                headerFilter: 'select',
+                headerFilterParams: {
+                    values: true,
+                    sortValuesList: 'asc'
+                }
+            };
+
+        case 'Rarity':
+            return {
+                title: field,
+                field: field,
+                headerTooltip: 'The rarity of the card',
+                responsive: 10,
+                widthGrow: 1,
+                headerFilter: 'select',
+                headerFilterParams: {
+                    values: true,
+                    sortValuesList: 'asc'
+                }
+            };
+
+        case 'Col':
+            return {
+                title: 'Have',
+                field: field,
+                headerTooltip: 'If I have the card or not',
+                responsive: 0,
+                widthGrow: 0.5,
+                headerFilter: 'tickCross',
+                formatter: 'tickCross',
+                hozAlign: 'center',
+                visible: false,
+                mutator: value => {
+                    switch (value) {
+                        case 'x': /* Own */
+                        case 'X': /* Own */
+                        case 'XX': /* Own */
+                        case 'XXX': /* Own */
+                        case 'XXXX': /* Own */
+                        case 'b': /* Bought */
+                        case 'B': /* Bought */
+                            return true;
+
+                        case 'o': /* Want */
+                        case 'O': /* Want */
+                        case '0': /* Want */
+                            return false;
+
+                        default:
+                            return null;
+                    }
+                }
+            };
+
+        case 'Dup':
+            return {
+                title: 'Stock',
+                field: field,
+                headerTooltip: 'The number of cards I have available to trade',
+                responsive: 0,
+                widthGrow: 0.5,
+                visible: false
+            };
+    }
+}
+
+function enableFilter(tablulator: Tabulator): void {
+    const want = document.getElementById('filter-want') as HTMLInputElement;
+    const trade = document.getElementById('filter-trade') as HTMLInputElement;
+    const have = document.getElementById('filter-have') as HTMLInputElement;
+    const all = document.getElementById('filter-all') as HTMLInputElement;
+
+    want.addEventListener('change', () => {
+        tablulator.clearFilter(/* includeHeaderFilters: */ false);
+        tablulator.setFilter("Col", "=", false)
+        tablulator.hideColumn('Col');
+        tablulator.hideColumn('Dup');
+        tablulator.redraw();
+    });
+
+    trade.addEventListener('change', () => {
+        tablulator.clearFilter(/* includeHeaderFilters: */ false);
+        tablulator.setFilter("Dup", ">", 0)
+        tablulator.hideColumn('Col');
+        tablulator.showColumn('Dup');
+        tablulator.redraw();
+    });
+
+    have.addEventListener('change', () => {
+        tablulator.clearFilter(/* includeHeaderFilters: */ false);
+        tablulator.setFilter("Col", "=", true)
+        tablulator.hideColumn('Col');
+        tablulator.hideColumn('Dup');
+        tablulator.redraw();
+    });
+    
+    all.addEventListener('change', () => {
+        tablulator.clearFilter(/* includeHeaderFilters: */ false);
+        tablulator.showColumn('Col');
+        tablulator.showColumn('Dup');
+        tablulator.redraw();
+    });
+
+    want.disabled = false;
+    trade.disabled = false;
+    have.disabled = false;
+    all.disabled = false;
+}
+
+function enableDownload(tablulator: Tabulator): void {
+    const download = document.getElementById('download') as HTMLButtonElement;
+    
+    download.addEventListener('click', () => {
+        tablulator.download("csv", downloadFileName());
+    });
+
+    download.disabled = false;
+}
+
+function downloadFileName(): string {
+    const name = document.getElementById('game-name').innerText;
+    const selectedList = (document.querySelector('#filter input:checked + label') as HTMLLabelElement).innerText;
+
+    return `Crash Dive - ${selectedList} (${name}).csv`;
 }
